0907ab76851cc5f7740668dc94f3e83865c7a5c8
Zyaire <zyairelai@gmail.com>
2022-01-08 10:26:03 +0800

    Update trade_isolated.py

--- trade_isolated.py
+++ trade_isolated.py
@@ -20,22 +20,27 @@ def lets_make_some_money():
             api_binance.market_hedge_open(pair, quantity)
 
         if api_binance.LONG_SIDE(response) == "LONGING":
-            if float(response[1].get('unRealizedProfit')) / float(response[1].get('isolatedMargin')) * 100 > config.take_profit_percentage:
-                api_binance.market_close_long(pair, response)
+            unrealizedPnL = float(response[1].get('unRealizedProfit')) / (float(response[1].get('entryPrice')) * abs(float(response[1].get('positionAmt'))) / int(response[2].get('leverage'))) * 100
+            # print(unrealizedPnL)
+            if unrealizedPnL > config.take_profit_percentage: api_binance.market_close_long(pair, response)
             else: print(colored("_LONG_SIDE : HOLDING_LONG", "green"))
 
         if api_binance.LONG_SIDE(response)  == "LONGING"  and api_binance.SHORT_SIDE(response) == "NO_POSITION" : print("SHORT_SIDE : 🐺 WAIT 🐺")
         if api_binance.SHORT_SIDE(response) == "SHORTING" and api_binance.LONG_SIDE(response)  == "NO_POSITION" : print("_LONG_SIDE : 🐺 WAIT 🐺")
 
         if api_binance.SHORT_SIDE(response) == "SHORTING":
-            if float(response[2].get('unRealizedProfit')) / float(response[2].get('isolatedMargin')) * 100 > config.take_profit_percentage:
-                api_binance.market_close_short(pair, response)
+            unrealizedPnL = float(response[2].get('unRealizedProfit')) / (float(response[2].get('entryPrice')) * abs(float(response[2].get('positionAmt'))) / int(response[2].get('leverage'))) * 100
+            # print(unrealizedPnL)
+            if unrealizedPnL > config.take_profit_percentage: api_binance.market_close_short(pair, response)
             else: print(colored("SHORT_SIDE : HOLDING_SHORT", "red"))
 
         print("Last action executed @ " + datetime.now().strftime("%H:%M:%S") + "\n")
     
 print(colored("LIVE TRADE IS ENABLED\n", "green")) if config.live_trade else print(colored("THIS IS A SHOWCASE\n", "red")) 
 
-scheduler = BlockingScheduler()
-scheduler.add_job(lets_make_some_money, 'cron', second='0', timezone=utc)
-scheduler.start()
+if config.live_trade:
+    scheduler = BlockingScheduler()
+    scheduler.add_job(lets_make_some_money, 'cron', second='0', timezone=utc)
+    scheduler.start()
+else:
+    lets_make_some_money()
